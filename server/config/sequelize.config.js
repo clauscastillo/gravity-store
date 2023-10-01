c3f2daaf511522ea12a47b3f7166c740bbe11cde
Claus Castillo <xclauscastillox@gmail.com>
2023-10-01 20:51:46 -0300

    RDS test

--- server/config/sequelize.config.js
+++ server/config/sequelize.config.js
@@ -1,13 +1,15 @@
 const { Sequelize } = require("sequelize");
 
+// Configura la instancia de Sequelize
 const sequelize = new Sequelize({
   host: "database-1.cxfqvjlozrcw.sa-east-1.rds.amazonaws.com", // Reemplaza con el endpoint de tu instancia RDS
   dialect: "mysql",
   username: "admin", // Reemplaza con tu nombre de usuario de RDS
-  password: "HY8AdW7KJEHv7995Ub2Hm68qEFv", // Reemplaza con tu contraseña de RDS
+  password: "6F3zLLXYRncEW8vUHW555tffnt7", // Reemplaza con tu contraseña de RDS
   database: "gravity", // Reemplaza con el nombre de tu base de datos
 });
 
+// Prueba la conexión
 sequelize
   .authenticate()
   .then(() => {
@@ -17,4 +19,4 @@ sequelize
     console.error("Error en la conexión a la base de datos:", error);
   });
 
-module.exports = sequelize;
+// Define tus modelos y realiza otras operaciones de Sequelize aquí.
